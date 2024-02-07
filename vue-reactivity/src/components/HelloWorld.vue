d8855870820a4ebbe5a590f17553d304a36b0989
OctavioM3 <octaviom3@nycstudents.net>
2024-02-07 09:23:13 -0500

    fjjfjfhj

---
+++ vue-reactivity/src/components/HelloWorld.vue
@@ -0,0 +1,22 @@
+<template>
+<button @click="increment">count is: {{ count }}</button>
+  <input v-model="text" placeholder="type here">
+  <p>{{ text }}</p>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const count = ref(0)
+//This adds a counter
+function increment(){
+  count.value++
+}
+
+const text = ref('')
+
+</script>
+
+<style scoped>
+
+</style>
